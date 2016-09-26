e024b5c6d3a3821e8c8cf119e9b47f461c5ba3eb
Shawn Hubbart <shawn.hubbart@gmail.com>
2016-09-26 15:46:00 -0700

    included formulas

--- MathLib/Test.h
+++ MathLib/Test.h
@@ -1,3 +1,41 @@
 #pragma once
 
-void doNothing(int x);
+int doNothing(int x);
+
+// Problem a:
+float test_quad(float x);
+
+//Problem b:
+
+// Note: Create structures whenever you feel they'll help!
+struct quad_results
+{
+	float sub_root, add_root;
+	int roots; //0, 1, 2
+};
+
+quad_results quad(float a, float b, float c);
+
+
+// problem c.
+float lerp(float start, float end, float t);
+
+// problem d.
+struct Point { float x, y; };
+float distance(const Point &P1, const Point &P2);
+
+
+// problem e.
+struct Point3D { float x, y, z; };
+float inner(const Point3D &P1, const Point3D &P2);
+
+
+// problem f.
+// Point-Plane Distance....
+struct Plane { Point3D abc; float d; };
+float point_plane_distance(const Plane &pl, const Point3D &pt);
+
+// problem g.
+// Bezier Curve....*/
+Point3D bezier(float t, const Point3D &p1, const Point3D &p2,
+	const Point3D &p3, const Point3D &p4);
--- MathTests/main.cpp
+++ MathTests/main.cpp
@@ -5,8 +5,34 @@
 
 int main()
 {
-	printf_s("Hello World! %d", doNothing(40));
+	//Unit Test
+	assert(doNothing(0) == 5);
+	assert(doNothing(-1) == 4);
+	assert(doNothing(1) == 8);
+
+	assert(test_quad(0) == -7);
+	assert(test_quad(-1) == -8);
+	assert(test_quad(1) == -4);
+
+	assert(quad(1, 0, 0).roots == 1);
+	assert(quad(1, -2, 0).roots == 2);
+	assert(quad(1, 0, 4).roots == 0);
+
+	assert(lerp(1, 0, 0) == 1);
+	assert(lerp(2, -2, 0) == 2);
+	assert(lerp(1, 2, 4) == 5);
+
+	assert(inner({ 1,1,0 }, { -1,1,0 }) == 0);
+	assert(inner({ 1,1,1 }, { 1,1,1 }) == 3);
+
+	assert(point_plane_distance({ 0,0,1,0 }, { 0,0,0 }) == 0);
+	assert(point_plane_distance({ 0,0,1,0 }, { 0,0,1 }) == 1);
+
+	assert(bezier(0, { 0,0,0 }, { 1,0,0 }, { 2,0,0 }, { 3,0,0 }).x == 0);
+	assert(bezier(1, { 0,0,0 }, { 1,0,0 }, { 2,0,0 }, { 3,0,0 }).x == 3);
+	assert(bezier(0.5f, { 0,0,0 }, { 1,0,0 }, { 2,0,0 }, { 3,0,0 }).x == 1.5);
 
 	getchar();
 	return 0;
 }
+
